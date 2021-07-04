d80d0db20fe883af5c1170cfba2ad4a42157b434
Tan Tran <tantranstst@gmail.com>
2021-07-04 14:38:46 +0700

    update db

--- dtb_mobifone v2.sql
+++ dtb_mobifone v2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2021 at 02:32 PM
+-- Generation Time: Jul 04, 2021 at 09:37 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -206,6 +206,63 @@ INSERT INTO `tbl_data_service` (`data_service_id`, `data_service_name`, `data_se
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_product`
+--
+
+CREATE TABLE `tbl_product` (
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `product_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `product_quantity` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `product_desc` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_price` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_images` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_status` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+INSERT INTO `tbl_product` (`product_id`, `product_name`, `category_id`, `product_quantity`, `brand_id`, `product_desc`, `product_content`, `product_price`, `product_images`, `product_status`, `created_at`, `updated_at`) VALUES
+(15, 'iphone 12', 4, 10, 3, '<ul>\r\n	<li>\r\n	<p>OLED6.1&quot;Super Retina XDR</p>\r\n	</li>\r\n	<li>\r\n	<p>Hệ điều h&agrave;nh:</p>\r\n\r\n	<p>iOS 14</p>\r\n	</li>\r\n	<li>\r\n	<p>Camera sau:</p>\r\n\r\n	<p>2 camera 12 MP</p>\r\n	</li>\r\n	<li>\r\n	<p>Camera trước:</p>\r\n\r\n	<p>12 MP</p>\r\n	</li>\r\n	<li>\r\n	<p>Chip:</p>\r\n\r\n	<p>Apple A14 Bionic</p>\r\n	</li>\r\n	<li>\r\n	<p>RAM:</p>\r\n\r\n	<p>4 GB</p>\r\n	</li>\r\n	<li>\r\n	<p>Bộ nhớ trong:</p>\r\n\r\n	<p>64 GB</p>\r\n	</li>\r\n	<li>\r\n	<p>SIM:</p>\r\n\r\n	<p>1 Nano SIM &amp; 1 eSIMHỗ trợ 5G</p>\r\n	</li>\r\n	<li>\r\n	<p>Pin, Sạc:</p>\r\n\r\n	<p>2815 mAh20 W</p>\r\n	</li>\r\n</ul>', '<h3>Trong những th&aacute;ng cuối năm 2020,&nbsp;<a href=\"https://www.thegioididong.com/apple\" target=\"_blank\" title=\"Tham khảo sản phẩm chính hãng của Apple tại Thế Giới Di Động\">Apple</a>&nbsp;đ&atilde; ch&iacute;nh thức giới thiệu đến người d&ugrave;ng cũng như iFan thế hệ iPhone&nbsp;12&nbsp;series&nbsp;mới với h&agrave;ng loạt t&iacute;nh năng bứt ph&aacute;, thiết kế được lột x&aacute;c ho&agrave;n to&agrave;n, hiệu năng đầy mạnh mẽ v&agrave; một trong số đ&oacute; ch&iacute;nh l&agrave;&nbsp;<a href=\"https://www.thegioididong.com/dtdd/iphone-12\" target=\"_blank\" title=\"Tham khảo điện thoại iPhone 12 chính hãng tại Thế Giới Di Động\">iPhone 12 64GB</a>.</h3>\r\n\r\n<h3>Hiệu năng vượt xa mọi giới hạn</h3>\r\n\r\n<p>Apple đ&atilde; trang bị con chip mới nhất của h&atilde;ng (t&iacute;nh đến 11/2020) cho iPhone 12 đ&oacute; l&agrave;&nbsp;<a href=\"https://www.thegioididong.com/hoi-dap/tim-hieu-ve-chip-apple-a14-bionic-tren-iphone-12-va-ipad-1290695\" target=\"_blank\" title=\"Tìm hiểu chip A14 Bionic là gì?\">A14 Bionic</a>, được sản xuất tr&ecirc;n tiến tr&igrave;nh 5 nm với hiệu suất ổn định hơn so với chip A13 được trang bị tr&ecirc;n phi&ecirc;n bản tiền nhiệm&nbsp;<a href=\"https://www.thegioididong.com/dtdd/iphone-11\" target=\"_blank\" title=\"Tham khảo điện thoại iPhone 11 chính hãng tại Thế Giới Di Động\">iPhone 11</a>.</p>\r\n\r\n<p><a href=\"https://www.thegioididong.com/images/42/213031/iphone-12-144220-044259.jpg\" onclick=\"return false;\"><img alt=\"Chip A14 Bionic mạnh mẽ | iPhone 12\" src=\"https://cdn.tgdd.vn/Products/Images/42/213031/iphone-12-144220-044259.jpg\" title=\"Chip A14 Bionic mạnh mẽ | iPhone 12\" /></a></p>\r\n\r\n<p>Xem th&ecirc;m:&nbsp;<a href=\"https://www.thegioididong.com/hoi-dap/tim-hieu-ve-chip-apple-a14-bionic-tren-iphone-12-va-ipad-1290695\" target=\"_blank\" title=\"Tìm hiểu về chip Apple A14 Bionic trên iPhone 12 và iPad Air 2020\">T&igrave;m hiểu về chip Apple A14 Bionic tr&ecirc;n iPhone 12 v&agrave; iPad Air 2020</a></p>\r\n\r\n<p>Với CPU Apple A14 Bionic, bạn c&oacute; thể dễ d&agrave;ng trải nghiệm mọi tựa game với những pha chuyển cảnh mượt m&agrave; hay h&agrave;ng loạt hiệu ứng đồ họa tuyệt đẹp ở mức đồ họa cao m&agrave; kh&ocirc;ng lo t&igrave;nh trạng giật lag.</p>', '15990000', '3a536745e4392618d2eef9634a66f20b6.jpg', 0, NULL, NULL),
+(16, 'iphone11', 4, 2, 3, '<p>sadvsdvsadv</p>', '<p>sadvsdvsdv</p>', '20000000', 'confusednickyoung-1608526031074136992496983.jpg', 0, NULL, NULL),
+(17, 'iphone x', 4, 4, 3, '<p>asfdsd</p>', '<p>sdvsdvdsv</p>', '20000000', 'm3-157407031889323267291837.jpg', 0, NULL, NULL),
+(18, 'iphone 7', 4, 10, 3, '<p>asff</p>', '<p>vsdvsdv</p>', '2690000', 'lefkbovz26vjxwqb6fmyue5hwojpuueesfaign5q85.jpeg', 0, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_service`
+--
+
+CREATE TABLE `tbl_service` (
+  `service_id` int(10) UNSIGNED NOT NULL,
+  `service_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `service_images` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `service_content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `service_status` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tbl_service`
+--
+
+INSERT INTO `tbl_service` (`service_id`, `service_name`, `service_images`, `service_content`, `service_status`, `created_at`, `updated_at`) VALUES
+(11, 'MCA', 'tải xuống2.jpg', '<p>Dịch vụ Th&ocirc;ng b&aacute;o cuộc gọi nhỡ (MCA) của MobiFone gi&uacute;p bạn biết th&ocirc;ng tin về c&aacute;c cuộc gọi nhỡ tới số thu&ecirc; bao của m&igrave;nh khi điện thoại di động của bạn đang tắt m&aacute;y, hết pin hoặc ngo&agrave;i v&ugrave;ng phủ s&oacute;ng....</p>', 0, NULL, NULL),
+(13, 'Game Data', 'm3-157407031889323267291811.jpg', '<p>Game data l&agrave; g&oacute;i cước cho ph&eacute;p thu&ecirc; bao của MobiFone miễn ph&iacute; data tốc độ cao truy cập chơi game của c&aacute;c Nh&agrave; ph&aacute;t h&agrave;nh Game lớn tại Việt Nam (danh s&aacute;ch game chi tiết tại data.mobifone.vn/game)</p>', 0, NULL, NULL),
+(14, 'Tvplay', 'm3-157407031889323267291867.jpg', '<p>L&agrave; dịch vụ gi&uacute;p kh&aacute;ch h&agrave;ng MobiFone xem trực tiếp c&aacute;c k&ecirc;nh truyền h&igrave;nh, xem c&aacute;c nội dung theo y&ecirc;u cầu v&agrave; c&oacute; thể thực hiện tương t&aacute;c (trả lời c&acirc;u hỏi, dự đo&aacute;n kết quả,..) trong khi xem c&aacute;c nội dung của dịch vụ ngay tr&ecirc;n thiết bị di động (điện thoại v&agrave; m&aacute;y t&iacute;nh bảng).</p>', 0, NULL, NULL),
+(15, 'iflix', 'lefkbovz26vjxwqb6fmyue5hwojpuueesfaign5q79.jpeg', '<p>Video data Iflix l&agrave; g&oacute;i cước cho ph&eacute;p thu&ecirc; bao của MobiFone kh&ocirc;ng giới hạn dung lượng tốc độ cao truy cập xem phim tr&ecirc;n iflix.com v&agrave; ứng dụng Iflix. Kh&aacute;ch h&agrave;ng c&oacute; thể d&ugrave;ng song song c&ugrave;ng c&aacute;c g&oacute;i data kh&aacute;c.</p>', 0, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -274,6 +331,18 @@ ALTER TABLE `tbl_data_service`
   ADD PRIMARY KEY (`data_service_id`);
 
 --
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `tbl_service`
+--
+ALTER TABLE `tbl_service`
+  ADD PRIMARY KEY (`service_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -325,6 +394,18 @@ ALTER TABLE `tbl_category`
 --
 ALTER TABLE `tbl_data_service`
   MODIFY `data_service_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  MODIFY `product_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `tbl_service`
+--
+ALTER TABLE `tbl_service`
+  MODIFY `service_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
